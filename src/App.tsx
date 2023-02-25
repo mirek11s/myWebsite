9723c1d7268fdc1018568a2739a2ce703dd9d8ed
Miroslav <miroslavsabev11@gmail.com>
2023-02-25 11:28:44 +0200

    webpack config setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import './styles.css'
+import IMAGE from './main_img.jpg';
+import LOGO from './svgFile.svg';
+
 export const App = () => {
     return (
         <div>
             <h1>My App</h1>
+            <img src={LOGO} alt="main image" />
+            <img src={IMAGE} alt="main image" width="300" height="200" />
         </div>
     );
 };
