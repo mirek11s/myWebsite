ea9bfd5d56462b8676e3ea34d0977a4165eae457
Miroslav <miroslavsabev11@gmail.com>
2023-02-27 12:01:16 +0200

    configuring tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './styles.css'
 import IMAGE from './main_img.jpg'
 import LOGO from './svgFile.svg'
 import { ClickCounter } from './components/ClickCounter'
+import './styles.css'
 
 export const App = () => {
   return (
     <div>
-      <h1>
+      <h1 className="text-primary">
         My App {process.env.NODE_ENV} - {process.env.name}
       </h1>
       <img src={LOGO} alt="main" />
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{ts,tsx,js,jsx}', './src/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1B73E8',
+      },
+    },
+  },
+  plugins: [],
+}
