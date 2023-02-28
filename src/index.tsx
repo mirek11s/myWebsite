30e1becf9f466dfce3a83b54789b2fb9b101139e
Miroslav <miroslavsabev11@gmail.com>
2023-02-28 19:19:41 +0200

    button improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
-import ReactDom from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-ReactDom.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
