0a9dcec63ea80477646da49db80339dd536f55d7
Miroslav <miroslavsabev11@gmail.com>
2023-03-01 23:02:22 +0200

    adding MainScreen changes

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,27 +1,21 @@
 import React from 'react'
 import TitleHeader from './TitleHeader'
+import PathButtons from './PathButtons'
+import IMAGE from '../main_img.jpg'
 
 const MainScreen = () => {
   return (
-    <>
-      <div className="my-8">
+    <div className="grid grid-cols-1">
+      <div className="col-span-1 my-8">
         <TitleHeader title="Hello World" />
       </div>
-      <div className="h-96 flex max-sm:flex-col items-center justify-center">
-        <button
-          type="button"
-          className="drop-shadow-xl text-white bg-gradient-to-br from-purple-800 to-blue-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-xl rounded-lg px-14 py-6 text-center mx-5 mb-2"
-        >
-          CODE
-        </button>
-        <button
-          type="button"
-          className="drop-shadow-xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium text-xl rounded-lg px-14 py-6 text-center mx-5 mb-2 max-sm:mt-6"
-        >
-          CLAY
-        </button>
+      <div className="col-span-1">
+        <PathButtons />
       </div>
-    </>
+      <div className="col-span-1">
+        <img src={IMAGE} alt="Image22" className="w-full object-contain" />
+      </div>
+    </div>
   )
 }
 
